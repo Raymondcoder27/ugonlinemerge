2cb934ee4447167ed855afc994c9d0e5dd098e79
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-22 11:53:44 +0300

    renames file domains

--- src/agentadmindomain/finances/FloatRequests.vue
+++ src/agentadmindomain/finances/FloatRequests.vue
@@ -431,7 +431,7 @@ onMounted(() => {
                 <!-- <td> -->
                 <!-- <label> -->
                 <span
-                  class="text-xs cursor-pointer rounded-md px-1 py-0.5 font-semibold text-white bg-blue-600 hover:text-white hover:bg-blue-700"
+                  class="text-xs cursor-pointer rounded-md px-1 py-0.5 font-semibold text-white bg-blue-600 hover:text-blue-700 hover:bg-blue-200"
                 >
                   <!-- <i class="fa-solid fa-check"></i>
                   Approved</span
@@ -466,7 +466,7 @@ onMounted(() => {
               <div v-else>
                 <!-- <td> -->
                 <span
-                  class="text-xs rounded-md px-1 py-0.5 font-semibold text-white bg-blue-600 hover:text-white hover:bg-blue-200 hover:text-blue-700"
+                  class="text-xs rounded-md px-1 py-0.5 font-semibold text-white bg-blue-600 hover:bg-blue-200 hover:text-blue-700"
                   @click="approveFloatRequest(request.id)"
                 >
                   <i class="fa-solid fa-check"></i>
